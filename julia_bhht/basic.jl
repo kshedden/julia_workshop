6d71ba1282753fa1965b0488fec211861c0be305
Kerby Shedden <kshedden@umich.edu>
2022-08-14 22:55:37 -0400

    update

---
+++ julia_bhht/basic.jl
@@ -0,0 +1,165 @@
+#==
+BHHT: Brief History of Human Time
+
+General information about the project:
+
+https://medialab.github.io/bhht-datascape/
+
+Download the data file from here (cross-verified-database.csv.gz)
+Use `wget` to obtain the data file if you are in a terminal.
+
+https://data.sciencespo.fr/dataset.xhtml?persistentId=doi:10.21410/7E4/RDAG3O
+
+The datafile must be in the same directory as this script.
+==#
+
+# Packages needed to run this script.  You may need to install
+# these packages (press `]` in the REPL then `add DataFrames`, etc.)
+using DataFrames, CSV, UnicodePlots, Loess, Random, Statistics
+
+# Load a small subset of data.  This is a "do" block
+# that automatically closes the io handle after reading.
+da = open("cross-verified-database.csv.gz") do io
+    CSV.read(io, DataFrame) #; limit=500000)
+end
+
+#==
+Below are many simple one liners.  The output will be lost when running this
+script, but you can type each of the lines below at the prompt to see the
+output.
+==#
+
+# The number of rows and columns
+size(da)
+
+# The number of rows
+nrow(da)
+
+# The number of columns
+ncol(da)
+
+# The column names
+names(da)
+
+# The column types
+eltype.(eachcol(da))
+
+# The first birth year
+minimum(skipmissing(da[:, :birth]))
+
+# The number of missing values
+count(ismissing.(da[:, :birth]))
+
+# The number of non-missing values
+count(.!ismissing.(da[:, :birth]))
+
+# The first and last death year
+extrema(skipmissing(da[:, :death]))
+
+# The unique regions
+unique(da[:, :un_region])
+
+# The number of rows per region
+combine(groupby(da, :un_region), nrow)
+
+#==
+More advanced pivot tables
+
+`unstack(df, u, v)` creates a pivot table whose columns are the distinct values
+of `u` and whose values are in the variable `v`.  The remaining columns define
+rows of the pivot table. If some combinations of rows and columns do not occur,
+use `allowmissing=true`, and the resulting elements of the pivot table will
+be missing.
+==#
+
+cc1 = combine(groupby(da, [:un_region, :gender]), nrow)
+ct1 = unstack(cc1, :gender, :nrow; allowmissing=true)
+
+cc2 = combine(groupby(da, [:un_region, :level1_main_occ, :gender]), nrow)
+ct2 = unstack(cc2, :un_region, :nrow; allowmissing=true)
+
+# Replace missing values in the pivot table with zero
+for c in eachcol(ct2)
+    if eltype(c) <: Number
+        replace!(c, missing=>0)
+    end
+end
+
+#==
+Analyses involving lifespan, a quantitative variable
+==#
+
+# Calculate the lifespan of each person in years
+da[:, :lifespan] = da[:, :death] - da[:, :birth]
+
+# Visualize the distribution of lifespans
+v = skipmissing(da[:, :lifespan])
+v = collect(v)
+plt1 = histogram(v)
+
+# Visualize the distributions of lifespans by gender
+db = filter(r->!ismissing(r.gender), da)
+u = groupby(db, :gender)
+v = [collect(skipmissing(x[:, :lifespan])) for x in u]
+t = [first(x[:, :gender]) for x in u]
+plt2 = boxplot(t, v)
+
+# Who is the man who lived for over 150 years?
+ii = findall(skipmissing(da[:, :lifespan] .> 150))
+
+# Consider the relationship between year of birth and lifespan
+# using loess, which doesn't handle missing values
+dx = da[:, [:birth, :lifespan, :gender]]
+dx = dx[completecases(dx), :]
+
+# Loess requires float type inputs
+for a in [:birth, :lifespan]
+    dx[!, a] = Float64.(dx[:, a])
+end
+
+# To work around censoring, consider people born before 1910
+dz = filter(r->r.birth <= 1910, dx)
+
+# Plot the conditional mean of lifespan given birth
+m = loess(dz[:, :birth], dz[:, :lifespan])
+xs = range(extrema(dz[:, :birth])...)
+ys = Loess.predict(m, xs)
+plt3 = scatterplot(xs, ys, canvas=BlockCanvas)
+
+# Consider only last 1000 years where most of the data are,
+# then plot the conditional mean of lifespan given birth
+dx = filter(r->r.birth >= 1000, dx)
+m = loess(dz[:, :birth], dz[:, :lifespan])
+xs = range(extrema(dz[:, :birth])...)
+ys = predict(m, xs)
+plt4 = scatterplot(xs, ys, canvas=BlockCanvas)
+
+# Estimate the conditional means of lifespan given
+# birth for females and males separately
+rr = []
+for sex in ["Female", "Male"]
+    dxx = filter(r->r.gender == sex, dx)
+    m = loess(dxx[:, :birth], dxx[:, :lifespan])
+    xs = range(extrema(dxx[:, :birth])...)
+    ys = predict(m, xs)
+    push!(rr, [xs, ys])
+end
+
+# Plot the sex-specific conditional mean lifespans
+plt5 = lineplot(rr[1]..., name="Female", xlim=[1000, 1900])
+lineplot!(plt5, rr[2]..., name="Male")
+
+# Summary statistics of lifespan for people born in each century
+da[:, :birth_century] = 100 .* floor.(da[:, :birth] ./ 100)
+gda = groupby(da, :birth_century)
+cc = combine(gda, nrow, :lifespan=>x->mean(skipmissing(x)))
+cc = rename(cc, :lifespan_function=>:lifespan_mean)
+uu = combine(gda, :lifespan=>x->std(skipmissing(x)))
+cc[:, :lifespan_std] = uu[:, :lifespan_function]
+
+db = da[:, [:gender, :birth_century, :un_region, :level1_main_occ]]
+db = db[completecases(db), :]
+db = filter(r->r[:birth_century] > 1000, db)
+rr = combine(groupby(db, [:birth_century, :un_region, :level1_main_occ, :gender]), nrow)
+rr = unstack(rr, :level1_main_occ, :nrow)
+
---
+++ julia_bhht/mca.jl
@@ -0,0 +1,193 @@
+using LinearAlgebra, StatsBase, StableRNGs, UnicodePlots, Printf
+using DataFrames, CSV, CategoricalArrays
+
+#==
+https://personal.utdallas.edu/~herve/Abdi-MCA2007-pretty.pdf
+==#
+
+mutable struct MCA
+
+    # The data matrix
+    Z::Array
+
+    # The embedding dimension
+    d::Int
+
+    # The indicator matrix
+    X::Array{Float64}
+
+    # Map values to integer codes
+    rd::Vector{Dict}
+
+    # Map integer codes to values
+    dr::Vector{Dict}
+
+    # Object scores
+    F::AbstractArray
+
+    # Variable scores
+    G::Vector{AbstractArray}
+end
+
+function MCA(Z, d)
+
+    # Get the indicator matrix
+    X, rd, dr = make_indicators(Z)
+    X ./= sum(X)
+
+    # Center the indicator matrix
+    r = sum(X, dims=2)[:]
+    c = sum(X, dims=1)[:]
+    Xc = X - r * c'
+
+    # Standardize the indicator matrix
+    Dr = Diagonal(1 ./ sqrt.(r))
+    Dc = Diagonal(1 ./ sqrt.(c))
+    Xz = Dr * Xc * Dc
+
+    # Get the object factor scores (F) and variable factor scores (G).
+    P, D, Q = svd(Xz)
+    F = Dr * P * Diagonal(D)
+    G = Dc * Q * Diagonal(D)
+
+    # Reduce to the requested dimension
+    F = F[:, 1:d]
+    G = G[:, 1:d]
+
+    # Split the variable scores to a separate array for each
+    # variable.
+    K = [length(di) for di in rd]
+    Js = cumsum(K)
+    Js = vcat(1, 1 .+ Js)
+    Gv = Vector{Matrix{Float64}}()
+    for j in 1:length(K)
+        g = G[Js[j]:Js[j+1]-1, :]
+        push!(Gv, g)
+    end
+
+    return MCA(Z, d, X, rd, dr, F, Gv)
+end
+
+
+function make_single_indicator(z)
+
+    n = length(z)
+
+    # Unique values of the variable
+    uq = sort(unique(z))
+
+    # Number of unique values of the variable
+    m = length(uq)
+
+    # Recoding dictionary, maps each distinct value to
+    # an offset
+    rd = Dict{eltype(z),Int}()
+    for (j, v) in enumerate(uq)
+        rd[v] = j
+    end
+
+    # The indicator matrix
+    X = zeros(n, m)
+    for (i,v) in enumerate(z)
+        X[i, rd[v]] = 1
+    end
+
+    # Reverse the recoding dictionary
+    rdi = Dict{Int,eltype(z)}()
+    for (k,v) in rd
+       rdi[v] = k
+    end
+
+    return X, rd, rdi
+end
+
+function make_indicators(Z)
+
+    rd, rdr = [], []
+    XX = []
+    for j in 1:size(Z, 2)
+        X, di, dir = make_single_indicator(Z[:, j])
+        push!(rd, di)
+        push!(rdr, dir)
+        push!(XX, X)
+    end
+    XX = hcat(XX...)
+
+    return XX, rd, rdr
+end
+
+function plot(UnicodePlots::Module, mca::MCA; x=1, y=2, kwargs...)
+
+    plt = scatterplot(mca.G[1][:, x], mca.G[1][:, y]; kwargs...)
+
+    for (j, g) in enumerate(mca.G)
+        dr = mca.dr[j]
+        for (k, v) in dr
+            annotate!(plt, g[k, x], g[k, y], string(v))
+        end
+    end
+
+    return plt
+end
+
+function test1()
+
+    rng = StableRNG(312)
+
+    n = 2000
+    Z = Matrix{String}(undef, n, 3)
+    Z[:, 1] = sample(rng, ["A", "B"], n)
+    for i in 1:n
+        if Z[i, 1] == "A"
+            Z[i, 2] = sample(rng, ["1", "2", "3"], Weights([0.8, 0.1, 0.1]))
+            Z[i, 3] = sample(rng, ["X", "Y", "Z"], Weights([0.45, 0.45, 0.1]))
+        elseif Z[i, 1] == "B"
+            Z[i, 2] = sample(rng, ["1", "2", "3"], Weights([0.1, 0.1, 0.8]))
+            Z[i, 3] = sample(rng, ["X", "Y", "Z"], Weights([0.45, 0.1, 0.45]))
+        else
+            error("!!")
+        end
+    end
+
+    mca = MCA(Z, 2)
+    plt = plot(UnicodePlots, mca; height=20, width=60)
+    println(plt)
+
+end
+
+#test1()
+
+#==
+Use MCA to analyze the BHHT data
+==#
+
+da = open("cross-verified-database.csv.gz") do io
+    CSV.read(io, DataFrame)
+end
+
+dd = da[:, [:birth, :level1_main_occ, :gender]]
+dd = dd[completecases(dd), :]
+
+# Remove very rare categories
+dd = filter(r->r.gender in ["Female", "Male"], dd)
+dd = filter(r->!ismissing(r.level1_main_occ) && !(r.level1_main_occ in ["Missing", "Other"]), dd)
+
+# Create 10 approximately equal-sized bins
+dd[:, :era] = cut(dd[:, :birth], 10)
+dd = select(dd, Not(:birth))
+
+dd = Matrix(dd)
+
+# Clean up the year labels
+f = function(x)
+    m = match(r"(-{0,1}\d{4}).*(-{0,1}\d{4})", x)
+    y = m.captures
+    y = parse.(Int, y)
+    return @sprintf("%d-%d", y[1], y[2])
+end
+dd[:, 3] = f.(dd[:, 3])
+
+mca = MCA(dd, 3)
+plt1 = plot(UnicodePlots, mca; width=90, height=25, xlim=[-3, 3])
+plt2 = plot(UnicodePlots, mca; x=1, y=3, width=90, height=25, xlim=[-3, 3])
+plt3 = plot(UnicodePlots, mca; x=2, y=3, width=90, height=25, xlim=[-3, 3])
